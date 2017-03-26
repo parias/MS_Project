1d0eef8e7f0d8e81b3d52a2bcc36a418ac358984
Pablo Arias <paarias24@gmail.com>
2017-03-26 02:11:33 -0400

    Changed rnn.py

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -1,53 +1,11 @@
 """
 http://monik.in/a-noobs-guide-to-implementing-rnn-lstm-using-tensorflow/
 """
+import sys
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from random import shuffle
-
-""" Creates random test/training set
-Creates binary numbers and shuffles them.
-Converts Binary number to np.array()
-Output_layer is how many 1's in input array,
-    but in 'one hot encoded'
-    3 = [0][0][0][1][0]...
-    1 in array.position[3]
-
-
-train_input = ['{0:020b}'.format(i) for i in range(2**20)]
-shuffle(train_input)
-train_input = [map(int,i) for i in train_input]
-ti = []
-for i in train_input:
-    temp_list = []
-    for j in i:
-        temp_list.append([j])
-    ti.append(np.array(temp_list))
-
-train_input = ti
-train_output = []
-
-for i in train_input:
-    count = 0
-    for j in i:
-        if j[0] == 1:
-            count += 1
-    temp_list = ([0]*21)
-    temp_list[count] = 1
-    train_output.append(temp_list)
-
-NUM_EXAMPLES = 10000
-test_input = train_input[NUM_EXAMPLES:]
-test_output = train_output[NUM_EXAMPLES:]
-
-train_input = train_input[:NUM_EXAMPLES]
-train_output = train_output[:NUM_EXAMPLES]
-"""
-
-# Create Real Test/Training set
-
-# Test set import using Pandas
 
 train_input = []
 train_output = []
@@ -81,53 +39,61 @@ with open('./Data/test.csv') as f:
 data = tf.placeholder(tf.float32, [None, 9,1])
 target = tf.placeholder(tf.float32, [None, 22])
 
-num_hidden = 24
-cell = tf.nn.rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
+def rnn(batch_size, epoch, num_hidden):
+    # num_hidden = 24
+    cell = tf.nn.rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
 
-val, state = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
+    val, state = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
 
-val = tf.transpose(val, [1,0,2])
-last = tf.gather(val,int(val.get_shape()[0]) - 1)
+    val = tf.transpose(val, [1,0,2])
+    last = tf.gather(val,int(val.get_shape()[0]) - 1)
 
-weight = tf.Variable(tf.truncated_normal([num_hidden, int(target.get_shape()[1])]))
-bias = tf.Variable(tf.constant(0.1, shape=[target.get_shape()[1]]))
+    weight = tf.Variable(tf.truncated_normal([num_hidden, int(target.get_shape()[1])]))
+    bias = tf.Variable(tf.constant(0.1, shape=[target.get_shape()[1]]))
 
-prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
+    prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
 
-cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
+    cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
 
-optimizer = tf.train.AdamOptimizer()
-minimize = optimizer.minimize(cross_entropy)
+    optimizer = tf.train.AdamOptimizer()
+    minimize = optimizer.minimize(cross_entropy)
 
-mistakes = tf.not_equal(tf.argmax(target,1), tf.argmax(prediction, 1))
-error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+    mistakes = tf.not_equal(tf.argmax(target,1), tf.argmax(prediction, 1))
+    error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
-init_op = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init_op)
+    init_op = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init_op)
 
-batch_size = 200
+    # batch_size = 200
 
-no_of_batches = int(len(train_input)/batch_size)
-epoch = 5
+    no_of_batches = int(len(train_input)/batch_size)
+    # epoch = 5
 
-print('==========', 'Running', '==========')
+#    print('==========', 'Running', '==========')
 
-for i in range(epoch):
-    ptr = 0
-    for j in range(no_of_batches):
-        inp, out = train_input[ptr:ptr+batch_size], train_output[ptr:ptr+batch_size]
-        ptr += batch_size
-        sess.run(minimize, {data: inp, target:out})
-    #print('Epoch - ', str(i))
-incorrect = sess.run(error, {data:test_input, target:test_output})
+    for i in range(epoch):
+        ptr = 0
+        for j in range(no_of_batches):
+            inp, out = train_input[ptr:ptr+batch_size], train_output[ptr:ptr+batch_size]
+            ptr += batch_size
+            sess.run(minimize, {data: inp, target:out})
+        #print('Epoch - ', str(i))
+    incorrect = sess.run(error, {data:test_input, target:test_output})
 
-print('batch_size: ' , str(batch_size))
-print('epoch: ', str(epoch))
+#    print('batch_size: ' , str(batch_size))
+#    print('epoch: ', str(epoch))
 
-#print('Epoch {:2d} error {:3.1f}%'.format(i + 1, 100 * incorrect))
-print('num_hidden: ', str(num_hidden))
-print('accuracy: {:3.1f}%'.format((100 * (1- incorrect))))
-print('=============================')
+    #print('Epoch {:2d} error {:3.1f}%'.format(i + 1, 100 * incorrect))
+#    print('num_hidden: ', str(num_hidden))
+#    print('accuracy: {:3.1f}%'.format((100 * (1- incorrect))))
+    print(batch_size, epoch, num_hidden, '{:3.1f}'.format((100 * (1 - incorrect))), sep=',')
+# print('=============================')
 
-sess.close()
+    sess.close()
+
+if __name__ == '__main__':
+    batch_size = int(sys.argv[2])
+    epoch = int(sys.argv[1])
+    num_hidden = int(sys.argv[3])
+    rnn(batch_size, epoch, num_hidden)
